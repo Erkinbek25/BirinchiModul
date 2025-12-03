0de976c10911fe38a6d60f70a886661afb92a3b0
Erkinbek25 <erkinbekhasanov1@gmail.com>
2025-12-03 09:05:42 +0500

    second try

---
+++ Dars2/Program.cs
@@ -0,0 +1,272 @@
+﻿namespace BirinchiDarr1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        /*Console.Write(" Enter any number: ");
+
+        
+        int x = int.Parse(Console.ReadLine());
+        int asl = x;
+
+        bool res = false;
+        int y = 0;
+        int a = 0;
+
+        if ( x < 0)
+        {
+            res = false;
+        }
+        else if ( x >= 0)
+        {
+           while ( x > 0)
+            {
+                a = x % 10;
+                y = y *10 + a;
+                x = x / 10;
+            }
+           if ( asl == y)
+            {
+                res = true;
+            }
+          
+        }
+
+        Console.WriteLine(res);*/
+
+        // 1.1
+
+        /*int soat = 2;
+        int minut = 2 * 60;
+        Console.WriteLine("2 soat " + minut + " minutga teng bo'ladi");*/
+
+        // 1.2
+
+        /*int hafta = 5;
+        int soat = (hafta * 7) * 24;
+        Console.WriteLine(" 5 hafta " + soat + " soatga teng");*/
+
+        // 1.3 
+
+        /*int minut = 4;
+        int sekund = minut * 60;
+        Console.WriteLine(" 4 minut " + sekund +" sekundga teng ");*/
+
+        // 1.4 
+
+        /*int soat = 25;
+        int sekund = soat * 60 * 60;
+
+        Console.WriteLine(" 25 soat " + sekund + " sekundga teng");*/
+
+        // 1.5
+
+        /*int hafta = 150;
+        int soat = (hafta * 7) * 24;
+
+        Console.WriteLine(" 150 hafta " + soat + " soatga teng ");*/
+
+        // 1.6
+
+        /*int kun = 4;
+        int minut = kun * 24 * 60;
+
+        Console.WriteLine(" 4 kun " + minut +" minutga teng");*/
+
+        // 1.7
+
+        /*int soat = 8;
+        int sekund = soat * 60 * 60;
+
+        Console.WriteLine( " 8  soat " + sekund+" sekundga teng");*/
+
+
+        // 1.8
+
+        /*int soat = 24;
+        int kun = 24 / 24;
+
+        Console.WriteLine(" 24 soat " + kun + " kunga teng");*/
+
+        // 1.9
+
+        /*int kun = 24;
+        int hafta = kun / 7;
+        Console.WriteLine(  "24 kun " + hafta + " haftaga teng ");*/
+
+        // 1.12 
+
+        /*int sekund = 2419200;
+        int hafta = sekund / 60 / 60 / 24 / 7;
+        Console.WriteLine(hafta);*/
+
+        // 1.10
+
+        /*int mb = 12;
+        int bayt = mb * 1024 * 1024;
+        Console.WriteLine(bayt);*/
+
+        // 1.20
+
+        /*double mb = 120000;
+        double tb = mb / 1024 / 1024;
+        Console.WriteLine(tb);*/
+
+        // 1.25
+
+        /*int mb = 4;
+        int kb = 10;
+        int bayt = (mb * 1024 * 1024 * 8) + (kb * 1024 * 8);
+        Console.WriteLine(bayt);*/
+
+        //1.2.8
+
+        /*int a = 3;
+        int b = 4;
+        Console.WriteLine(a + b);*/
+
+
+        // 1.2.18
+
+        /*Console.WriteLine(" Minutni kriting");
+        int minut = int.Parse(Console.ReadLine());
+
+        int kun = minut / 60 / 24;
+        Console.WriteLine(kun);*/
+
+
+        // 1.2.23
+
+        /* Console.WriteLine("Son kriting");
+
+         int a = int.Parse(Console.ReadLine());
+
+         int a3 = a * 3;
+         int a4 = a * 4;
+         int a5 = a * 5;
+
+         Console.WriteLine(a3);
+         Console.WriteLine(a4);
+         Console.WriteLine(a5);*/
+
+        //1.3.10
+
+        /*Console.WriteLine("Enter any number");
+        int a = int.Parse(Console.ReadLine());
+
+        int birlar = a % 10;
+        int onlar = a / 10 % 10;
+        int yuzlar = a / 100;
+
+        int res = birlar * 100 + onlar * 10 + yuzlar;
+        Console.WriteLine(res);*/
+
+
+        // 1.3.17
+        /*
+        int gb = 12;
+        int res = gb / 2;
+        Console.WriteLine(res);*/
+
+        // 1.3.22
+
+        /*int a = 3;
+        int res = a << 3;
+        Console.WriteLine(res);*/
+
+        //1.4.6
+
+        /*int minut = 60;
+        int soat = 1;
+
+        int sekund = minut * 60 + soat * 60 * 60;
+        Console.WriteLine(sekund);*/
+
+
+        // 1.4.16
+        /*
+        int km = 1;
+        int m = 10;
+        int dm = m / 10 + km * 10;
+        Console.WriteLine(dm);*/
+
+        // 1.4.20
+
+        /*Console.WriteLine("Konfet kgsini kiriting:");
+        int konfetKGg = int.Parse(Console.ReadLine());
+        Console.WriteLine("Konfet narxini kiriting");
+        int konfetNarx = int.Parse(Console.ReadLine());
+        Console.WriteLine("Shokolad kgsini kriting");
+        int shokoladKG = int.Parse(Console.ReadLine());
+        Console.WriteLine("Shokolad narxini kriting");
+        int shokoladNarx = int.Parse(Console.ReadLine());
+
+
+        int konfet1KG = konfetNarx / konfetKGg;
+        int shokolad1KG = shokoladNarx / shokoladKG;
+
+        int res = shokolad1KG - konfet1KG;
+
+        Console.WriteLine(res);*/
+
+
+        // 1.5.11
+
+        /* Console.WriteLine("Enter any number");
+         int a = int.Parse(Console.ReadLine());
+
+         int birlar = a % 10;
+         int onlar = a / 10;
+         bool res = false;
+         if ( birlar == onlar)
+         {
+             res = true;
+         }
+
+         Console.WriteLine(res);*/
+
+
+        // 1.5.24
+
+        /*int a = 8;
+        int b = 7;
+
+        if ( a == b)
+        {
+            Console.WriteLine("true");
+        }
+        else Console.WriteLine("false");*/
+
+
+        // 1.5.33
+
+        /*int a = 7;
+        int b = 9;
+        int d = 4;
+        int c = 2;
+        
+        if ( c < a && c < b && c < d && c %2 == 0)
+        {
+            Console.WriteLine("True");
+        }
+        else Console.WriteLine("false");*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
